e0ab29b90b8c450989108554a7415022eceb178e
AlexandreRosset <alexandre.rosset97@laposte.net>
2018-11-16 09:50:20 +0100

    add index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+let http = require("http");
+let app = require('express')();
+let express = require('express');
+
+httpserver = http.createServer(app);
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+httpserver.listen(8081);
+
+console.log('Server running at http://127.0.0.1:8081/');
