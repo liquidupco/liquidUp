7ec2d79a678609c7f33dc6e884832a6d57419190
Rocknologie <theo.schmidt@outlook.fr>
2018-11-28 11:14:33 +0100

    Commit avant update

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 let http = require("http");
 let app = require('express')();
 let express = require('express');
+var router = express.Router();
 
 httpserver = http.createServer(app);
 
 app.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
+});
+
+router.get('/', function (req, res) {
+	res.sendfile(__dirname + '/theo.html');
 });
 
 httpserver.listen(8081);
