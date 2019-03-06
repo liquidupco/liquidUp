2f8974bda73f81d9b0fd839a2b897c5c0fee2b30
AlexandreRosset <alexandre.rosset97@laposte.net>
2019-03-06 15:18:48 +0100

    code pour la connexion dans index.html

--- public/index.js
+++ public/index.js
@@ -1,7 +1,4 @@
-//import axios from 'axios';
-
 function register() {
-
   axios
     .post('http://localhost:1337/auth/local/register', {
       username: document.forms["registration"]["username"].value,
@@ -13,13 +10,33 @@ function register() {
       console.log('Well done!');
       console.log('User profile', response.data.user);
       console.log('User token', response.data.jwt);
-      alert('user token:' + response.data.jwt);
+      alert('Compte connecté, user token:' + response.data.jwt);
     })
     .catch(error => {
       // Handle error.
       console.log('An error occurred:', error);
       alert('création de compte raté');
     });
+
+
 }
 
-// Request API.
+function login() {
+  axios
+    .post('http://localhost:1337/auth/local', {
+      identifier: document.forms["registration"]["username"].value,
+      password: document.forms["registration"]["password"].value
+    })
+    .then(response => {
+      // Handle success.
+      console.log('Well done!');
+      console.log('User profile', response.data.user);
+      console.log('User token', response.data.jwt);
+      alert('Compte connecté, user token:' + response.data.jwt);
+    })
+    .catch(error => {
+      // Handle error.
+      console.log('An error occurred:', error);
+      alert('Connexion de compte raté');
+    });
+}
